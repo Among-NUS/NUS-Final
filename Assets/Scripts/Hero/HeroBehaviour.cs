15d8b7ee30ea7f3966033d3d6172227e3ec011d6
Nobodiiiii <-@->
2025-07-07 17:06:05 +0800

    实现回溯

---
+++ Assets/Scripts/Hero/HeroBehaviour.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class HeroBehaviour : MonoBehaviour
+{
+    public float speed = 0.1f;
+
+    void FixedUpdate()
+    {
+        HandleMovement();
+
+        if (GameManager.Instance.IsRecording)
+            GameManager.Instance.RecordKeyInput(CollectKeyInputs());
+    }
+
+    void HandleMovement()
+    {
+        Vector3 move = Vector3.zero;
+        if (Input.GetKey(KeyCode.W)) move += Vector3.up;
+        if (Input.GetKey(KeyCode.S)) move += Vector3.down;
+        if (Input.GetKey(KeyCode.A)) move += Vector3.left;
+        if (Input.GetKey(KeyCode.D)) move += Vector3.right;
+
+        transform.position += move * speed;
+    }
+
+    List<char> CollectKeyInputs()
+    {
+        List<char> keys = new();
+        if (Input.GetKey(KeyCode.A)) keys.Add('a');
+        if (Input.GetKey(KeyCode.D)) keys.Add('d');
+        if (Input.GetKey(KeyCode.W)) keys.Add('w');
+        if (Input.GetKey(KeyCode.S)) keys.Add('s');
+        return keys;
+    }
+}
