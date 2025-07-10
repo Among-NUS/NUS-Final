22b9eb53a38abfcb5d3217574efdf0d762e36339
lcjy <lcjy00000001@163.com>
2025-07-10 22:57:32 +0800

    add menu

---
+++ Assets/Scripts/UI/MenuBehaviour.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuBehaviour : MonoBehaviour
+{
+    public void ClickStart()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+    public void ClickExit()
+    {
+        Debug.Log("quitGame");
+        Application.Quit();
+    }
+}
