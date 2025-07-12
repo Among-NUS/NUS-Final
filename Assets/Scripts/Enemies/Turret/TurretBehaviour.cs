18b5d8e2bbcce92bda792e26b311feaf0015f9aa
Nobodiiiii <-@->
2025-07-12 16:54:41 +0800

    LockedDoor-Prefab

--- Assets/Scripts/Enemies/Turret/TurretBehaviour.cs
+++ Assets/Scripts/Enemies/Turret/TurretBehaviour.cs
@@ -5,13 +5,13 @@ public class TurretBehaviour : MonoBehaviour, IInteractable
     private InteractionManager im;
     public Turret turret;
     private Shooter shooter;
-    private Monitor monitor;  // ← 添加监视器引用
+    private MonitorBehaviour monitor;  // ← 添加监视器引用
 
     void Awake()
     {
         turret = GetComponent<Turret>();
         shooter = GetComponentInChildren<Shooter>();
-        monitor = GetComponentInChildren<Monitor>();
+        monitor = GetComponentInChildren<MonitorBehaviour>();
     }
 
     public Transform GetTransform() => transform;
