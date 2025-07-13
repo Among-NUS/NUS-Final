bda068e0e0a3cfcb5a174225f09bee87baa2394a
Y4k23wl <2119394376@qq.com>
2025-07-13 23:13:58 +0800

    shooter has sound circle

--- Assets/Scripts/Utils/Shooter.cs
+++ Assets/Scripts/Utils/Shooter.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Shooter : MonoBehaviour
@@ -11,13 +12,22 @@ public class Shooter : MonoBehaviour
     private float fireCooldown = 0f;
     public AudioClip gunshot;
     private AudioSource shooterAudioS;
+    private GameObject soundSource = null;
+    private float lastSound = 0f;
+    public float soundCooldown = 0.1f;
+
 
     void Awake()
     {
         shooterAudioS = GetComponent<AudioSource>();
+        soundCooldown = 0.5f;
     }
     void FixedUpdate()
     {
+        if (soundSource != null&&Time.time-lastSound>soundCooldown)
+        {
+            Destroy(soundSource);
+        }
         if (fireCooldown > 0f)
             fireCooldown -= Time.fixedDeltaTime;
     }
@@ -30,7 +40,8 @@ public class Shooter : MonoBehaviour
         shooterAudioS.clip = gunshot;
         shooterAudioS.Play();
         Vector2 dir = faceLeft ? Vector2.left : Vector2.right;
-
+        soundSource = (GameObject)Instantiate(Resources.Load("Prefabs/soundSource"), firePoint.position+new Vector3(3.1f,0f,0f),Quaternion.identity);
+        lastSound = Time.time;
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
         BulletBehaviour bb = bullet.GetComponent<BulletBehaviour>();
         if (bb != null)
