b4fd6d864f5514c9fa10219a798ce8cab526181b
lcjy <lcjy00000001@163.com>
2025-07-16 16:21:31 +0800

    renew the shield

--- Assets/Scripts/Enemies/Enemy/Shield.cs
+++ Assets/Scripts/Enemies/Enemy/Shield.cs
@@ -7,7 +7,7 @@ public class Shield : MonoBehaviour
     [Header("need to drag")]
     public GameObject followedEnemy;
     [Header("auto update")]
-    public bool isHoldingShield = false;
+    public bool isHoldingShield = true;
     public bool isFaingLeft = false;
     public float distance = 1f;
     // Start is called before the first frame update
@@ -22,13 +22,14 @@ public class Shield : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        isHoldingShield = 
-            (EnemyBehaviour.EnemyState.CHASE == followedEnemy.GetComponent<Enemy>().enemyState)||
-            (EnemyBehaviour.EnemyState.SHOOT == followedEnemy.GetComponent<Enemy>().enemyState);
+        //isHoldingShield = 
+        //    (EnemyBehaviour.EnemyState.CHASE == followedEnemy.GetComponent<Enemy>().enemyState)||
+        //    (EnemyBehaviour.EnemyState.DISCOVER == followedEnemy.GetComponent<Enemy>().enemyState) ||
+        //    (EnemyBehaviour.EnemyState.SHOOT == followedEnemy.GetComponent<Enemy>().enemyState);
         //在射击或者追击时持盾
         isFaingLeft = followedEnemy.GetComponent<Enemy>().facingLeft;
         Vector3 positionBias = new Vector2(isFaingLeft?-1:1,0);
-        positionBias = isHoldingShield? positionBias : -positionBias;
+        positionBias = isHoldingShield? -positionBias : positionBias;
         transform.position = followedEnemy.transform.position+positionBias*distance;
         transform.localScale = new Vector2 (positionBias.x>0?1:-1, 1);
     }
