de6e47ca3723fb02adba3e7f3ba6efc7510828ce
Y4k23wl <2119394376@qq.com>
2025-07-16 20:45:11 +0800

    boss behaviour

--- Assets/BossBehaviour.cs
+++ Assets/BossBehaviour.cs
@@ -20,7 +20,7 @@ public class BossBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
     public void BossWalk(float duration,bool facingLeft)
